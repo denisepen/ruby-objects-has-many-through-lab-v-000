25a85589fd638dbdf99c5ecfc0039088e81edf71
Denise Pendleton <dpendl1@hotmail.com>
2018-03-09 02:29:44 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -6,7 +6,7 @@ class Appointment
     @date = date
     @appointment = appointment
     @doctor = doctor
-    patient.add_appointment = self
+    doctor.add_appointment = self
     @patient = patient
 end
 end
