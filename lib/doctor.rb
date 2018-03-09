bc4dc841a0cc552ed01290a6f9aa0c040284a0c3
Denise Pendleton <dpendl1@hotmail.com>
2018-03-09 01:56:02 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,9 +1,16 @@
 class Doctor
 
   attr_accessor :appointments, :name
+
   def initialize(name)
     @appointments = []
     @name = name
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+    
   end
 
 end
