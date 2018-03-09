e9926cd8af8410786337980c6fe7209379f6d45e
Denise Pendleton <dpendl1@hotmail.com>
2018-03-09 02:06:47 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -5,7 +5,7 @@ class Appointment
   def initialize(date, doctor)
     @date = date
     @appointment = appointment
-    @doctor - doctor
+    @doctor = doctor
     doctor.add_appointment = self
 end
   
